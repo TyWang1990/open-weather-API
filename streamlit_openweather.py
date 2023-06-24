3f2c147e9adc097230fd869b79408470e999c0fa
TyWang1990 <111158867+TyWang1990@users.noreply.github.com>
2023-06-24 22:21:44 +1200

    Update streamlit_openweather.py

--- streamlit_openweather.py
+++ streamlit_openweather.py
@@ -15,7 +15,7 @@ def get_weather_info(location_name):
     cur_temp=data_2['main']['temp']
     return f"The current temperature of {location_name} is: {cur_temp}°C", f"The current weather of {location_name} is: {cur_weather}"
 
-streamlit.title('🌁 Weather Data & Trends Dashboard')
+streamlit.title('🌁 Weather Data & Trends Dashboard 🌁 ')
 city_choice = streamlit.text_input('Which city would you like the check?', 'London')
 
 streamlit.text(get_weather_info(city_choice)[0])
