a5aa97e0dbf8bb5736752b85f4ff4925e92fc4ad
TyWang1990 <111158867+TyWang1990@users.noreply.github.com>
2023-07-03 20:17:36 +1200

    Update streamlit_openweather.py

--- streamlit_openweather.py
+++ streamlit_openweather.py
@@ -58,5 +58,51 @@ def color_background(value):
 
 streamlit.dataframe(df.style.applymap(color_background, subset=['temp', 'temp_min', 'temp_max']))
 
-cities_selected=streamlit.multiselect("Pick some cities: ", city_choice, ['Bandon', 'Bend'])
 
+
+
+def get_history_weather_info(location_name):
+    api_key='0d4be18c209e1127a2eb7dca54706dfa'
+    url_coordinates=f"http://api.openweathermap.org/geo/1.0/direct?q={location_name}&limit=1&appid={api_key}"
+    response_1=requests.get(url_coordinates)
+    data_1=response_1.json()
+    lat=data_1[0]["lat"]
+    lon=data_1[0]["lon"]
+    url_weather=f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}"
+    response_2=requests.get(url_weather)
+    data_2=response_2.json()
+    list_datetime=[]
+    list_temp_min=[]
+    list_temp_max=[]
+    for i in range(0,len(data_2['list'])):
+        list_datetime.append(data_2['list'][i]['dt_txt'])
+        list_temp_min.append(data_2['list'][i]['main']['temp_min'])
+        list_temp_max.append(data_2['list'][i]['main']['temp_max'])
+    keys=list_datetime
+    keys_2=['temp_min', 'temp_max']
+    dic_forecast={}
+    for i in keys:
+        for j in keys_2:
+            key=(i,j)
+            dic_forecast[key]=None
+    list_minmax = [val for pair in zip(list_temp_min, list_temp_max) for val in pair]
+    for index, key in enumerate(dic_forecast):
+        dic_forecast[key]=list_minmax[index]
+    dic_filtered={key: value for key, value in dic_forecast.items() if '09:00:00' in key[0]}
+    modified_dict = {}
+    for key, value in dic_filtered.items():
+        modified_key = (key[0].split()[0], key[1])
+        modified_dict[modified_key] = value
+    single_df = pd.DataFrame(modified_dict.values(), index=pd.MultiIndex.from_tuples(modified_dict.keys()))
+    single_df = single_df.rename(columns={0: location_name})
+    return single_df
+    
+cities_selected_2=streamlit.multiselect("Pick some cities: ", city_choice, ['Bandon', 'Bend'])
+
+concatenated_df = pd.DataFrame()
+
+for city in cities_selected_2:
+    temp_df=get_history_weather_info(i)
+    concatenated_df=pd.concat([temp_df, concatenated_df], axis=1)
+
+streamlit.dataframe(concatenated_df.style.applymap(color_background))
